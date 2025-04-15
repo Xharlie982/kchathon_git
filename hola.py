f32a7267141067c9a70363f68573597b5a5694ca
Carlos <carlos.quipse@utec.edu.pe>
2025-04-15 09:38:43 -0500

    Suma de 2 numeros

--- hola.py
+++ hola.py
@@ -1,1 +1,5 @@
-hola mundo 
+# Suma dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+suma = num1 + num2
+print(f"La suma es: {suma}")
